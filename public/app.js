baa8deb25e72af521d96ad435cdf76f77784ec0d
gdi2290 <github@gdi2290.com>
2013-06-26 10:54:05 -0700

    remove jade

--- public/app.js
+++ public/app.js
@@ -7,6 +7,7 @@
     Vent: _.extend({}, Backbone.Events),
     Router: {},
     initialize: function() {
+      new Redd.Views.App({model: new Redd.Models.App()});
       new Redd.Router();
      Backbone.history.start({pushState: true});
     }
