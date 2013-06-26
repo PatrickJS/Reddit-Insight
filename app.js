c803ecff3c96aca567f42b81db8f75fa994098f1
Alex Gaputin <googamanga@yahoo.com>
2013-06-26 00:17:43 -0700

    basic pullPosts funciton, add mongoose

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , _ = require('underscore')
+  , handlebars = require('handlebars')
+  , lib = require('./myLibrary')
+  , mongoose = require('mongoose');
 
 var app = express();
 
@@ -27,8 +31,33 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//routes
 app.get('/', routes.index);
 
+//mongoose
+mongoose.connect("mongodb://localhost/RedditInsight");
+
+//schema
+var 
+
+//pulling data
+app.throttledPullData = _.throttle(lib.getJSON, 2000);
+
+var options = {
+    host: 'www.reddit.com',
+    path: '/r/all/top.json?t=all&limit=100',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+//start server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+  setInterval( function(){
+    app.throttledPullData(options, function(statusCode, result){
+      console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+    })
+  }, 10000);
 });
