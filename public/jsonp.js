cdda2d1d3957734af0dc852c887a602d1919bcea
gdi2290 <github@gdi2290.com>
2013-06-27 17:20:41 -0700

    update with jsonp function

--- public/jsonp.js
+++ public/jsonp.js
@@ -1,10 +1,5 @@
-// Redd.JSONP.ParseUrl = function(where, user) {
-  // return 'http://www.reddit.com/'+where+'/'+user;
-// };
-
-// Redd.JSONP.Request = function(url, callback, args) {
-  // args = [].slice.call(arguments);
-  // var yolo = 'yolo' + ~~(Math.random()*8888);
-  // $('<script src="'+ url +'.json?jsonp='+yolo+'"></script>').appendTo('head');
-  // window[yolo] = callback;
-// };
+Redd.JSONP.Sync = function(method, model, options) {
+  options.dataType = 'jsonp';
+  options.jsonp = 'jsonp';
+  return Backbone.sync(method, model, options);
+};
