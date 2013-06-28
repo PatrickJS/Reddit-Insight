8d424abb74e352102bef3bcd046c6c1c4d322c8d
Eleanor <elle.beal@gmail.com>
2013-06-28 14:27:45 -0700

    Created model for posts graph

---
+++ public/d3Logic.js
@@ -0,0 +1,90 @@
+
+$(document).ready(function(){
+  var tv = 250;
+
+
+  var palette = new Rickshaw.Color.Palette();
+
+  var graph = new Rickshaw.Graph( {
+          element: document.querySelector("#chart"),
+          width: 540,
+          height: 240,
+          renderer: 'line',
+          series: new Rickshaw.Series.ExpandingDuration([{ name: 'Karma' }], undefined, {
+            timeInterval: tv,
+            maxDataPoints: 100,
+            timeBase: new Date().getTime() / 1000
+          })
+  });
+
+  var legend = new Rickshaw.Graph.Legend( {
+          element: document.querySelector('#legend'),
+          graph: graph
+  } );
+
+  var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+    graph: graph
+  } );
+
+  var ticksTreatment = 'glow';
+
+  var xAxis = new Rickshaw.Graph.Axis.Time( {
+    graph: graph,
+    ticksTreatment: ticksTreatment
+  } );
+
+  xAxis.render();
+
+  var yAxis = new Rickshaw.Graph.Axis.Y( {
+    graph: graph,
+    tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+    ticksTreatment: ticksTreatment
+  } );
+
+  yAxis.render();
+
+  // var slider = new Rickshaw.Graph.RangeSlider( {
+  //   graph: graph,
+  //   element: $('#slider')
+  // } );
+
+  var offsetForm = document.getElementById('offset_form');
+
+
+  graph.render();
+
+  var i = 0;
+  var iv = setInterval( function() {
+
+    //updated data pushed to this variable:
+    var data = { Karma: Math.floor(Math.random() * 200)+200};
+    //additional data dilineated by .two .three ...
+
+    // var randInt = Math.floor(Math.random()*100);
+    // data.two = (Math.sin(i++/40) + 4) * (randInt + 400);
+    // data.three = randInt + 300;
+
+    //add the data to the series
+    graph.series.addData(data);
+    //re render
+    graph.render();
+
+  //time offset:
+  }, tv );
+
+  //add toggles
+  offsetForm.addEventListener('change', function(e) {
+        var offsetMode = e.target.value;
+
+        if (offsetMode == 'lines') {
+                graph.setRenderer('line');
+                graph.offset = 'zero';
+        } else {
+                graph.setRenderer('stack');
+                graph.offset = offsetMode;
+        }
+        graph.render();
+
+  }, false);
+
+});
--- public/lib/d3Logic.js
+++ public/lib/d3Logic.js
@@ -1,30 +1,43 @@
-d3.json('/lib/userTestData.json', function(data) {
-      var chart = nv.models.lineChart();
-    nv.addGraph(function() {
+// d3.json('/lib/userTestData.json', function(data) {
+//       var chart = nv.models.lineChart();
+//     nv.addGraph(function() {
 
 
-      chart.xAxis
-          .axisLabel('Time (s)')
-          .tickFormat(d3.format(',r'));
+//       chart.xAxis
+//           .axisLabel('Time (s)')
+//           .tickFormat(d3.format(',r'));
 
-      chart.yAxis
-          .axisLabel('Karma (v)')
-          .tickFormat(d3.format('.02f'));
+//       chart.yAxis
+//           .axisLabel('Karma (v)')
+//           .tickFormat(d3.format('.02f'));
 
-      d3.select('.chart-container svg')
-          .datum(data)
-          .transition().duration(500).call(chart);
+//       d3.select('.chart-container svg')
+//           .datum(data)
+//           .transition().duration(500).call(chart);
 
-      nv.utils.windowResize(chart.update);
+//       nv.utils.windowResize(chart.update);
 
-      return chart;
-    });
-    Redd.redraw = function(){
+//       return chart;
+//     });
+//     Redd.redraw = function(){
 
-        d3.select('.chart-container svg')
-        .datum(data)
-        .transition().duration(4000).call(chart);
-        return chart.update;
-    };
- });
+//         d3.select('.chart-container svg')
+//         .datum(data)
+//         .transition().duration(4000).call(chart);
+//         return chart.update;
+//     };
+//  });
+$(document).ready(function(){
+  var data = [ { x: 0, y: 40 }, { x: 1, y: 49 }, { x: 2, y: 17 }, { x: 3, y: 42 } ];
 
+  var graph = new Rickshaw.Graph({
+          element: document.querySelector("#chart"),
+          width: 580,
+          height: 250,
+          series: [{
+                  color: 'steelblue',
+                  data: data
+          }]
+  });
+});
+graph.render();
