422941c0fc7098752ce3f828be1af2695b60bdc1
Kevin Smith <kevinhsmitty@gmail.com>
2013-07-01 14:45:40 -0700

    added footer view and css styling

--- public/redd.js
+++ public/redd.js
@@ -13,6 +13,7 @@
       window.Debug = {};
       Debug.Controller = new Redd.Controller();
       Debug.Controller.show('navbar');
+      Debug.Controller.show('footer');
       Debug.Router = new Redd.Router(Debug.Controller);
       Backbone.history.start(/*{pushState: true}*/);
     }
