fdf9ec278fd0a8a4c029a1388e7459ba80eefbf3
gdi2290 <github@gdi2290.com>
2013-07-02 23:52:38 -0700

    clean up dashboard

--- public/views/dashboard_view.js
+++ public/views/dashboard_view.js
@@ -1,16 +1,11 @@
 Redd.Views.Dashboard = Backbone.View.extend({
   initialize: function() {
-    console.log('in Dashboard view');
   },
   events: {
-    'click': function() {
-      alert("Clickified!");
-    }
   },
   el: '#dashboard',
   template: Redd.Templates('dashboard'),
   render: function() {
-    console.log('render dashboard view');
     this.$el.html(this.template());
     return this;
   }
