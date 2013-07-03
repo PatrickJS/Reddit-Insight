282e5f57f91547faa68e067d9f00815a3005caad
gdi2290 <github@gdi2290.com>
2013-07-03 00:12:31 -0700

    clean up wordcloud

--- public/models/wordcloud_model.js
+++ public/models/wordcloud_model.js
@@ -1,23 +1,10 @@
 Redd.Models.WordCloud = Backbone.Model.extend({
-<<<<<<< HEAD
   initialize: function() {
     this.set('rotateFunc', this.get(this.get('_rotateFuncChoice')),{silent: true});
     console.log('initializing');
-    this.url += "/GamingNoun?limit="+ this.get('count');
-
-    this.fetch({
-      success: function(model, res, options){
-        console.log('fetch success - res: ', res);
-      },
-      error: function(model, res, options){
-          console.log('fetch error - res: ', res);
-      }
-    });
+    this.base = this.url;
+    this.pull(this);
   },
-=======
-  renderCounter: 0,
-  url: '/api/wordClouds',
->>>>>>> 758fc4cbe737ad315b34fb7301d9be5007754eae
   defaults: {
     'baseUrl': null,
     'limit': 550,
@@ -26,69 +13,48 @@ Redd.Models.WordCloud = Backbone.Model.extend({
     'selectedSubreddit': 'GamingNoun',
     '_rotateFuncChoice': '_rotate90discrete',
     '_rotate180continuous': function() { return ~~(Math.random() * 180) - 90;},
-    '_rotate90discrete': function() { return ~~(Math.random() * 2) * 90; },
-<<<<<<< HEAD
-    'switchRotateFuncChoice': function(){
-
-      if( this.get('_rotateFuncChoice') === '_rotate180continuous'){
-        this.set('rotateFunc', this.get('_rotate90discrete'), {silent: true});
-        this.set('_rotateFuncChoice', '_rotate90discrete');
-      } else if( this.get('_rotateFuncChoice') === '_rotate90discrete'){
-        this.set('rotateFunc', this.get('_rotate180continuous'), {silent: true});
-        this.set('_rotateFuncChoice', '_rotate180continuous');
-      } else { throw "internal error: unexpected rotation functino selected";}
-    }
+    '_rotate90discrete': function() { return ~~(Math.random() * 2) * 90; }
   },
+  renderCounter: 0,
   url: '/api/wordClouds',
-=======
-  },
-  initialize: function() {
-    this.set('rotateFunc', this.get(this.get('_rotateFuncChoice')),{silent: true});
-    console.log('initializing');
-    this.base = this.url;
-    this.pull(this);
-  },
-  pull: function(self){
-    self.url = self.base + "/GamingNoun?limit="+ self.get('limit');
-    self.fetch({
-      success: function(model, res, options){
-        console.log('fetch success - res: ', res);
-      },
-      error: function(model, res, options){
-          console.log('fetch error - res: ', res);
-      }
-    });
-  },
-  update: function(obj){
-    this.set('limit', obj.limit,{silent:true});
-    this.set('sizeMultiple', obj.sizeMultiple,{silent:true});
-    if(this.get('_rotateFuncChoice') !== obj.viewType){
-      if( this.get('_rotateFuncChoice') === '_rotate180continuous'){
-        this.set('rotateFunc', this.get('_rotate90discrete'), {silent: true});
-        this.set('_rotateFuncChoice', '_rotate90discrete',{silent:true});
-      } else if( this.get('_rotateFuncChoice') === '_rotate90discrete'){
-        this.set('rotateFunc', this.get('_rotate180continuous'), {silent: true});
-        this.set('_rotateFuncChoice', '_rotate180continuous',{silent:true});
-      } else { throw "internal error: unexpected rotation functino selected"}
-    }
-    this.pull(this);
-  },
->>>>>>> 758fc4cbe737ad315b34fb7301d9be5007754eae
   parse: function(data) {
     // overwrite parse to reformat the data before backbone handles it
     var updatedAttributes = _.extend(this.attributes);
     updatedAttributes.wordArray = [];
     updatedAttributes.frequency = {};
-    for(var i = 0; i <= updatedAttributes.limit && i < data.length; i++){
+    for (var i = 0; i <= updatedAttributes.limit && i < data.length; i++) {
       updatedAttributes.wordArray.push(data[i].noun);
       updatedAttributes.frequency[data[i].noun] = Math.sqrt(+data[i].frequency) * this.get('sizeMultiple');
     }
     return updatedAttributes;
   },
 
-  renderCounter: 0,
-  parsedData: {
-
+  pull: function(self) {
+    self.url = self.base + "/GamingNoun?limit="+ self.get('limit');
+    self.fetch({
+      success: function(model, res, options){
+        console.log('fetch success - res: ', res);
+      },
+      error: function(model, res, options){
+        console.log('fetch error - res: ', res);
+      }
+    });
   },
+  update: function(obj) {
+    this.set('limit', obj.limit,{silent:true});
+    this.set('sizeMultiple', obj.sizeMultiple,{silent:true});
+    if (this.get('_rotateFuncChoice') !== obj.viewType) {
+      if ( this.get('_rotateFuncChoice') === '_rotate180continuous') {
+        this.set('rotateFunc', this.get('_rotate90discrete'), {silent: true});
+        this.set('_rotateFuncChoice', '_rotate90discrete',{silent:true});
+      } else if ( this.get('_rotateFuncChoice') === '_rotate90discrete') {
+        this.set('rotateFunc', this.get('_rotate180continuous'), {silent: true});
+        this.set('_rotateFuncChoice', '_rotate180continuous',{silent:true});
+      } else {
+        throw "internal error: unexpected rotation functino selected";
+      }
+    }
+    this.pull(this);
+  }
 });
 
