a95ccb25725cf84574a629446927ed9e4a5e1d65
gdi2290 <github@gdi2290.com>
2013-07-02 23:52:29 -0700

    clean up routes

--- public/router.js
+++ public/router.js
@@ -10,27 +10,22 @@ Redd.Router = Backbone.Router.extend({
   },
   dashboard: function(){
     this.controller.hide();
-    console.log('in dashboard route');
     this.controller.show('dashboard');
   },
   prediction: function(){
     this.controller.hide();
-    console.log('in prediction route');
     this.controller.show('prediction');
   },
   trackpost: function(url){
     this.controller.hide();
-    console.log('in trackPost route');
     this.controller.show('trackpost');
   },
   trackuser: function(url){
     this.controller.hide();
-    console.log('in trackUser route');
     this.controller.show('trackuser');
   },
   wordcloud: function(url){
     this.controller.hide();
-    console.log('in wordcloud route');
     this.controller.show('wordcloud');
   }
 });
