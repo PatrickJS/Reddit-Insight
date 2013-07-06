a6c64dc5f45b22dd66b114f8ad117050c78d60ec
Kevin Smith <kevinhsmitty@gmail.com>
2013-07-06 15:07:48 -0700

    animate and style first infographic section

--- public/views/dashboard_view.js
+++ public/views/dashboard_view.js
@@ -2,10 +2,13 @@ Redd.Views.Dashboard = Backbone.View.extend({
   initialize: function() {
     $(document).ready(function() {
       var controller = $.superscrollorama({
-        playoutAnimations: false
+        playoutAnimations: false,
+        reverse: false
       });
       controller.addTween('#infograph-cats', TweenMax.from( $('#infograph-cats'), .5, {css:{right:'1500px'}, ease:Quad.easeInOut}));
       controller.addTween('#infograph-cats-img', TweenMax.from( $('#infograph-cats-img'), .5, {css:{right:'1500px'}, ease:Quad.easeInOut}));
+      controller.addTween('#infograph-nsfw', TweenMax.from( $('#infograph-nsfw'), .5, {css:{right:'1500px'}, ease:Quad.easeInOut}));
+      controller.addTween('#infograph-nsfw-img', TweenMax.from( $('#infograph-nsfw-img'), .5, {css:{right:'1500px'}, ease:Quad.easeInOut}));
     });
   },
   events: {
