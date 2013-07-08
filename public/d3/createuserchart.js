b424f1cd30418104fd094663d5cfb0efd7d1c474
Eleanor <elle.beal@gmail.com>
2013-07-07 23:21:12 -0700

    Changes to user posts graph

--- public/d3/createuserchart.js
+++ public/d3/createuserchart.js
@@ -1,59 +1,60 @@
 Redd.d3.CreateUserChart = function(collection) {
   var bottomRange = 0,
       topRange = 200,
-      scaleRange = [bottomRange,topRange];
+      scaleRange = [bottomRange,topRange],
+      scale = [d3.scale.linear().domain(scaleRange).nice()];
 
   var dataParse = function(){
-        return _(collection.toJSON()).map(function(model){
-          return model.data;
+    var allPostData = [],
+        dataCount = 0;
+        _(collection.toJSON()).map(function(model){
+          if(model.kind === "t3"){
+            dataCount++;
+            var objectData = {
+              x : dataCount,
+              y : model.data.score
+            };
+            //model.data.title
+            //model.data.score
+            //model.data.created_utc
+            allPostData.push(objectData);
+          }
         });
+        return allPostData;
       };
-  var yolo = dataParse();
-  var scale = [d3.scale.linear().domain(scaleRange).nice()],
-      palette = new Rickshaw.Color.Palette();
 
-  var graph = new Rickshaw.Graph( {
-              element: document.querySelector("#user-chart"),
-              width: 540,
-              height: 240,
-              renderer: 'line',
-              series: new Rickshaw.Series.FixedDuration([{ name: 'Karma'}], yolo, {
-                timeInterval: 300,
-                maxDataPoints: 0,
-                timeBase: new Date().getTime() / 1000
-              })
-      });
+  var postData = dataParse();
 
-  // Redd.d3.legend = new Rickshaw.Graph.Legend( {
-  //         element: document.querySelector('#legend'),
-  //         graph: Redd.d3.graph
-  // } );
+  var graph = new Rickshaw.Graph({
+    element: document.querySelector("#user-chart"),
+    renderer: 'bar',
+    series: [{
+      data: postData,
+      color: 'steelblue'
+    }]
+  });
+  graph.render();
 
-  // Redd.d3.hoverDetail = new Rickshaw.Graph.HoverDetail( {
-  //   graph: Redd.d3.graph
-  // } );
+  hoverDetail = new Rickshaw.Graph.HoverDetail( {
+    graph: graph
+  } );
 
-  // var ticksTreatment = 'glow';
+  var ticksTreatment = 'glow';
 
-  // Redd.d3.xAxis = new Rickshaw.Graph.Axis.Time( {
-  //   graph: Redd.d3.graph,
-  //   ticksTreatment: ticksTreatment
-  // } );
+  xAxis = new Rickshaw.Graph.Axis.Time( {
+    graph: graph,
+    ticksTreatment: ticksTreatment
+  } );
 
-  // Redd.d3.xAxis.render();
+  xAxis.render();
 
-  // Redd.d3.yAxis = new Rickshaw.Graph.Axis.Y.Scaled( {
-  //   scale: Redd.d3.scale[0],
-  //   graph: Redd.d3.graph,
-  //   tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-  //   ticksTreatment: ticksTreatment
-  // } );
+  yAxis = new Rickshaw.Graph.Axis.Y.Scaled( {
+    scale: scale[0],
+    graph: graph,
+    tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+    ticksTreatment: ticksTreatment
+  } );
 
-  // Redd.d3.yAxis.render();
-
-  // var offsetForm = document.getElementById('offset_form');
-
-  //drop initial 0 value
-  graph.series.render();
+  yAxis.render();
 
 };
