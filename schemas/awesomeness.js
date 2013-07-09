44a1ec6bde9d1ac8aa1104a7dafebdd64a6340af
gdi2290 <github@gdi2290.com>
2013-07-09 06:44:34 -0700

    remove tabs

--- schemas/awesomeness.js
+++ schemas/awesomeness.js
@@ -1,13 +1,13 @@
 function fixStuff() {
-	var nouns = db.GamingAllNouns.find();
-	nouns.forEach(function(doc) {
-		db.GamingAllNouns.update({ _id: doc._id }, {
-			$set: {
-				noun: doc.nouns
-			},
-			$unset: {
-				nouns: 1
-			}
-		});
-	});
+  var nouns = db.GamingAllNouns.find();
+  nouns.forEach(function(doc) {
+    db.GamingAllNouns.update({ _id: doc._id }, {
+      $set: {
+        noun: doc.nouns
+      },
+      $unset: {
+        nouns: 1
+      }
+    });
+  });
 }
