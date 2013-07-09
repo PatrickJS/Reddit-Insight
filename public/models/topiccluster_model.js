27dd2a17729bde7e7297e67ab499ed57d83680d1
Alex Gaputin <googamanga@yahoo.com>
2013-07-09 14:03:32 -0700

    took out day_week graph

--- public/models/topiccluster_model.js
+++ public/models/topiccluster_model.js
@@ -12,7 +12,6 @@ Redd.Models.TopicCluster = Backbone.Model.extend({
   },
   update: function(obj){
     this.set('selectedSubreddit', obj.subreddit, {silent: true});
-    // this.set('path', 'TopicClusterData/' + obj.subreddit+ '.json');
     this.set('path', function(){
       console.log('getting path', 'TopicClusterData/' + obj.subreddit+ '.json');
       return 'TopicClusterData/' + obj.subreddit+ '.json';
