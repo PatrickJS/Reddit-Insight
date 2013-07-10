a6a78ded7198b3a54fa874b12341d0548c0b570e
gdi2290 <github@gdi2290.com>
2013-07-10 15:42:45 -0700

    made the server router more clear

--- public/router.js
+++ public/router.js
@@ -1,28 +1,31 @@
 Redd.Router = Backbone.Router.extend({
-  initialize: function(control) {
-    this.controller = control;
+  initialize: function(controller) {
+    this.controller = controller;
   },
   handleRoute: function(routeName) {
     routeName = routeName || 'dashboard';
     this.controller.hide();
-    // if (!routeName) { routeName = 'dashboard'; }
 
     switch(routeName) {
-    case 'trackuser':
-    case 'trackpost':
-    case 'topiccluster':
-    case 'graphs':
-    case 'interaction':
-      // Fixed width
-      this.controller.hideFull();
-      this.controller.showPages();
-      break;
-    default:
-      // 100% width
-      this.controller.hidePages();
-      this.controller.showFull();
-      break;
+      case 'trackuser':
+      case 'trackpost':
+      case 'topiccluster':
+      case 'graphs':
+      case 'interaction':
+        // Fixed width
+        this.controller.hideFull();
+        this.controller.showPages();
+        break;
+      case 'frequency':
+      case 'circlecluster':
+      case 'dashboard':
+      case 'wordcloud':
+        // 100% width
+        this.controller.hidePages();
+        this.controller.showFull();
+        break;
     }
+
     this.controller.show(routeName);
   },
   routes: {
