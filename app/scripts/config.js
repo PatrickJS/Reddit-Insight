9fcacae7bec569c9796b33a4c3e00f0d92c9768a
gdi2290 <github@gdi2290.com>
2013-10-02 19:36:03 -0700

    add more routes

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -8,7 +8,35 @@ angular.module('RedditInsightApp')
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/wordcloud', {
+        templateUrl: 'views/visualizations/wordcloud.html',
+        controller: 'MainCtrl'
+      })
+      .when('/topiccluster', {
+        templateUrl: 'views/visualizations/topiccluster.html',
+        controller: 'MainCtrl'
+      })
+      .when('/circlecluster', {
+        templateUrl: 'views/visualizations/circlecluster.html',
+        controller: 'MainCtrl'
+      })
+      .when('/interaction', {
+        templateUrl: 'views/visualizations/interaction.html',
+        controller: 'MainCtrl'
+      })
+      .when('/frequency', {
+        templateUrl: 'views/visualizations/frequency.html',
+        controller: 'MainCtrl'
+      })
+      .when('/graphs', {
+        templateUrl: 'views/visualizations/graphs.html',
+        controller: 'MainCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  .config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+  })
+;
