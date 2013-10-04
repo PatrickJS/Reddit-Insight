50023e1f0feeb6f10555a0354d98027b51d72b38
gdi2290 <github@gdi2290.com>
2013-10-04 15:36:26 -0700

    add seo support

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -42,6 +42,6 @@ angular.module('RedditInsightApp')
       });
   })
   .config(function($locationProvider) {
-    // $locationProvider.html5Mode(true);
+    // $locationProvider.html5Mode(true).hashPrefix('!');;
   })
 ;
