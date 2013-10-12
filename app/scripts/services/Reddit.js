ff4463292e93d590c6a3c9a517c7698ea57f52d9
gdi2290 <github@gdi2290.com>
2013-10-12 13:18:25 -0700

    update Reddit service to new module pattern

--- app/scripts/services/Reddit.js
+++ app/scripts/services/Reddit.js
@@ -1,16 +1,26 @@
+;(function(app) {
 'use strict';
 
-angular.module('services')
-  .factory('Reddit', function() {
-    var reddit = {};
+app.factory('Reddit', function($http, Tracking) {
+  var reddit = {};
 
-    reddit.trackPost = function() {
-      $http
-        .jsonp('http://reddit.com/.json?jsonp=JSON_CALLBACK')
-        .success()
-        .error()
-    }
+  reddit.limit = 100
 
-    return reddit
-  })
-;
+  reddit.trackPost = function(postUrl) {
+    var fullUrl;
+    postUrl
+      .replace('www.', 'pay.')
+      .replace('http://', 'https://');
+
+    fullUrl = postUrl + '.json?jsonp=JSON_CALLBACK&limit=' + this.limit;
+
+    return $http.jsonp(fullUrl)
+      .success(Tracking.success('trackpost', fullUrl))
+      .error(Tracking.error('trackpost', fullUrl))
+  }
+
+  return reddit
+});
+
+
+}(angular.module('services')));
