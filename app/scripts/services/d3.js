4bc15313f2aed2191bd7f368942bc7874b54f95e
gdi2290 <github@gdi2290.com>
2013-10-12 10:11:37 -0700

    refactor D3 service

--- app/scripts/services/d3.js
+++ app/scripts/services/d3.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('services')
-  .factory('$d3', ['$document', '$q', '$rootScope', '$window',
-    function($document, $q, $rootScope, $window) {
-      var d = $q.defer();
+  .factory('D3', ['$q', '$rootScope', '$window',
+    function($q, $rootScope, $window) {
+      var deferred = $q.defer()
+          $document = $window.document,
+          _d3 = $window.d3;
       function init() {
         function onScriptLoad() {
           // Load client in the browser
-          $rootScope.$apply(function() { d.resolve(window.d3); });
+          $rootScope.$apply(function() {
+            deferred.resolve($window.d3);
+          });
         }
         // Create a script tag with d3 as the source
         // and call our onScriptLoad callback when it
@@ -16,7 +20,7 @@ angular.module('services')
         scriptTag.type = 'text/javascript';
         scriptTag.async = true;
         scriptTag.src = 'http://d3js.org/d3.v3.min.js';
-        scriptTag.onreadystatechange = function () {
+        scriptTag.onreadystatechange = function() {
           if (this.readyState == 'complete') {
             onScriptLoad();
           }
@@ -26,14 +30,10 @@ angular.module('services')
         var s = $document[0].getElementsByTagName('body')[0];
         s.appendChild(scriptTag);
 
-        return d.promise;
+        return deferred.promise;
       }
-
-      return {
-        d3: $window.d3,
-        promise: function() {
-
-          return $window.d3 || init();
-        }
+      _d3.promise = function() {
+        return $window.d3 || init();
       };
+      return _d3;
 }]);
