7e92610737cc3e978de4c5d9da02840007fe124c
gdi2290 <github@gdi2290.com>
2013-10-12 14:53:16 -0700

    add TrackUserCtrl controller

---
+++ app/scripts/controllers/trackuser.js
@@ -0,0 +1,26 @@
+;(function(app) {
+'use strict';
+
+app.controller('TrackUserCtrl', function($scope, Reddit) {
+
+  $scope.usernameState = false;
+
+  $scope.username = '';
+
+  $scope.redditData = {}
+
+  $scope.submitUser = function(username) {
+    console.log('username ', username);
+    Reddit.trackUser(username).then(function(response) {
+      console.log('trackuserctrl: ', response);
+      $scope.redditData.user = response.data.data;
+      $scope.usernameState = true;
+    });
+  };
+
+
+});
+
+
+
+}(angular.module('controllers')));
