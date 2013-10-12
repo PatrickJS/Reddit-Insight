1fe69293b64d2b48235be7603cb41b828b8a1cb8
gdi2290 <github@gdi2290.com>
2013-10-12 13:26:11 -0700

    second pass refactor D3 service

--- app/scripts/services/d3.js
+++ app/scripts/services/d3.js
@@ -1,39 +1,48 @@
+;(function(app) {
 'use strict';
 
-angular.module('services')
-  .factory('D3', ['$q', '$rootScope', '$window',
-    function($q, $rootScope, $window) {
-      var deferred = $q.defer()
-          $document = $window.document,
-          _d3 = $window.d3;
-      function init() {
-        function onScriptLoad() {
-          // Load client in the browser
-          $rootScope.$apply(function() {
-            deferred.resolve($window.d3);
-          });
-        }
-        // Create a script tag with d3 as the source
-        // and call our onScriptLoad callback when it
-        // has been loaded
-        var scriptTag = $document[0].createElement('script');
-        scriptTag.type = 'text/javascript';
-        scriptTag.async = true;
-        scriptTag.src = 'http://d3js.org/d3.v3.min.js';
-        scriptTag.onreadystatechange = function() {
-          if (this.readyState == 'complete') {
-            onScriptLoad();
-          }
-        }
-        scriptTag.onload = onScriptLoad;
+app.factory('D3',function($q, $rootScope, $window) {
 
-        var s = $document[0].getElementsByTagName('body')[0];
-        s.appendChild(scriptTag);
+  if ($window.d3) {
+    var _d3 = $window.d3;
+  } else {
+    var deferred = $q.defer()
+    var $document = $window.document;
+    init();
+  }
 
-        return deferred.promise;
+  function init() {
+    function onScriptLoad() {
+      // Load client in the browser
+      $rootScope.$apply(function() {
+        _d3 = $window.d3;
+        deferred.resolve($window.d3);
+      });
+    }
+    // Create a script tag with d3 as the source
+    // and call our onScriptLoad callback when it
+    // has been loaded
+    var scriptTag = $document[0].createElement('script');
+    scriptTag.type = 'text/javascript';
+    scriptTag.async = true;
+    scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+    scriptTag.onreadystatechange = function() {
+      if (this.readyState == 'complete') {
+        onScriptLoad();
       }
-      _d3.promise = function() {
-        return $window.d3 || init();
-      };
-      return _d3;
-}]);
+    }
+    scriptTag.onload = onScriptLoad;
+
+    var s = $document[0].getElementsByTagName('body')[0];
+    s.appendChild(scriptTag);
+
+    return deferred.promise;
+  }
+  _d3.load = function() {
+    return $window.d3 || init();
+  };
+
+  return _d3;
+});
+
+}(angular.module('services')));
