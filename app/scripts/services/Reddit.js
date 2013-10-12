66b483420a2836934133674a48cbd60f0008c33e
gdi2290 <github@gdi2290.com>
2013-10-12 14:52:50 -0700

    add trackUser and userOverview to reddit service

--- app/scripts/services/Reddit.js
+++ app/scripts/services/Reddit.js
@@ -2,22 +2,38 @@
 'use strict';
 
 app.factory('Reddit', function($http, Tracking) {
-  var reddit = {};
 
-  reddit.limit = 100
+  var reddit = {
+    limit: 100,
+    trackPost: function(postUrl) {
+      var fullUrl;
+      postUrl
+        .replace('www.', 'pay.')
+        .replace('http://', 'https://');
 
-  reddit.trackPost = function(postUrl) {
-    var fullUrl;
-    postUrl
-      .replace('www.', 'pay.')
-      .replace('http://', 'https://');
+      fullUrl = postUrl + '.json?jsonp=JSON_CALLBACK&limit=' + this.limit;
 
-    fullUrl = postUrl + '.json?jsonp=JSON_CALLBACK&limit=' + this.limit;
+      return $http.jsonp(fullUrl)
+        .success(Tracking.success('trackpost', fullUrl))
+        .error(Tracking.error('trackpost', fullUrl))
+    },
+    trackUser: function(username) {
+      var fullUrl = 'https://pay.reddit.com/user/'+ username +'/about.json?jsonp=JSON_CALLBACK';
 
-    return $http.jsonp(fullUrl)
-      .success(Tracking.success('trackpost', fullUrl))
-      .error(Tracking.error('trackpost', fullUrl))
-  }
+      return $http.jsonp(fullUrl)
+        .success(Tracking.success('trackuser', fullUrl))
+        .error(Tracking.error('trackuser', fullUrl))
+    },
+    userOverview: function(username) {
+      var fullUrl = 'https://pay.reddit.com/user/' + username + '/overview.json?jsonp=JSON_CALLBACK&limit=' + this.limit;
+
+      return $http.jsonp(fullUrl)
+        .success(Tracking.success('trackpost', fullUrl))
+        .error(Tracking.error('trackpost', fullUrl))
+    }
+
+
+  };
 
   return reddit
 });
