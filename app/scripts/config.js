26ae20cbeaca2e967510ee602db0d05371c3ae9d
gdi2290 <github@gdi2290.com>
2013-10-12 13:17:44 -0700

    update config to new module pattern

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,48 +1,47 @@
+;(function(app) {
 'use strict';
 
 /* Config */
-angular.module('RedditInsightApp')
-  .config( function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html'
-      })
-      .when('/trackpost', {
-        templateUrl: 'views/trackpost.html',
-        controller: 'TrackPostCtrl as post'
-      })
-      .when('/trackuser', {
-        templateUrl: 'views/trackuser.html'
-      })
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html'
+    })
+    .when('/trackpost', {
+      templateUrl: 'views/trackpost.html',
+      controller: 'TrackPostCtrl as post'
+    })
+    .when('/trackuser', {
+      templateUrl: 'views/trackuser.html'
+    })
+    .when('/wordcloud', {
+      templateUrl: 'views/visualizations/wordcloud.html'
+    })
+    .when('/topiccluster', {
+      templateUrl: 'views/visualizations/topiccluster.html'
+    })
+    .when('/circlecluster', {
+      templateUrl: 'views/visualizations/circlecluster.html'
+    })
+    .when('/interaction', {
+      templateUrl: 'views/visualizations/interaction.html'
+    })
+    .when('/frequency', {
+      templateUrl: 'views/visualizations/frequency.html'
+    })
+    .when('/graphs', {
+      templateUrl: 'views/visualizations/graphs.html'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+})
+.config(function($locationProvider) {
+  // $locationProvider.html5Mode(true).hashPrefix('!');;
+});
 
-      .when('/wordcloud', {
-        templateUrl: 'views/visualizations/wordcloud.html'
-      })
-      .when('/topiccluster', {
-        templateUrl: 'views/visualizations/topiccluster.html'
-      })
-      .when('/circlecluster', {
-        templateUrl: 'views/visualizations/circlecluster.html'
-      })
-      .when('/interaction', {
-        templateUrl: 'views/visualizations/interaction.html'
-      })
-      .when('/frequency', {
-        templateUrl: 'views/visualizations/frequency.html'
-      })
-      .when('/graphs', {
-        templateUrl: 'views/visualizations/graphs.html'
-      })
-
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-  .config( function($locationProvider) {
-    // $locationProvider.html5Mode(true).hashPrefix('!');;
-  })
-;
+}(angular.module('RedditInsightApp')));
