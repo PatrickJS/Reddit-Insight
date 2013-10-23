6fede54e1e277d2edaf1f9eaf145fd0597dc6d2c
gdi2290 <github@gdi2290.com>
2013-10-22 20:01:16 -0700

    add isActive function for active tab

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
  angular.module('controllers')
-  .controller('MainCtrl', function($scope) {
-
+  .controller('MainCtrl', function($scope, $location) {
+    $scope.isActive = function(route) {
+      var path = $location.path().split('/');
+      path = '/'+path[1];
+      return route === path;
+    }
   });
