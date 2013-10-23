88c237321d21c8c754f6411579d26eb4545ea485
gdi2290 <github@gdi2290.com>
2013-10-22 20:02:03 -0700

    refactor trackPost

--- app/scripts/controllers/trackpost.js
+++ app/scripts/controllers/trackpost.js
@@ -1,55 +1,47 @@
 ;(function(app) {
 'use strict';
 
-app.controller('TrackPostCtrl', function($scope, $interval, Reddit) {
+app.controller('TrackPostCtrl', function($scope, $location, $timeout, Reddit) {
 
-  $scope.urlState = false;
-
-  $scope.trackingUrl = '';
+  $scope.trackingUrl = 'http://www.reddit.com/r/javascript/comments/'+$scope.$routeParams.post+'/'+$scope.$routeParams.title;
 
   $scope.redditData = {}
 
-  $scope.intervalTime = 3000;
+  $scope.intervalTime = 0;
 
   $scope.cancelPolling = function(urlState) {
-
-    intervalStop();
-    $scope.urlState = !urlState;
-
+    console.log('cancel');
+    $timeout.cancel(prevousTimeout);
+    $location.path('/trackpost');
   };
 
-  $scope.submitLink = function(url) {
 
-    $scope.$log.info('link ', url);
+  $scope.$on('$locationChangeStart' , function() {
+    console.log('location change start');
+    $timeout.cancel(prevousTimeout);
+  });
 
-    intervalStop();
-    intervalStart(url);
+  var prevousTimeout = null;
+  startPolling($scope.trackingUrl);
 
+  function startPolling(url) {
+    prevousTimeout = $timeout(function() {
 
-  };
-
-  var prevousInterval = null;
-
-  function intervalStop() {
-
-    $interval.cancel(prevousInterval);
-
-  }
-
-  function intervalStart(url) {
-
-    $scope.urlState = true;
-
-    var prevousInterval = $interval(function() {
       Reddit.trackPost(url).then(function(response) {
         $scope.$log.info('trackpostctrl: ', response);
 
         $scope.redditData = response.data[0].data.children[0].data
         $scope.$emit('trackpost', url);
+        $scope.intervalTime = 3000;
+        $timeout.cancel(prevousTimeout);
+        startPolling(url);
+        return this;
       });
+
     }, $scope.intervalTime);
 
   }
+
 
 });
 
