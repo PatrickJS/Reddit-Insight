846b50839d5ac74fa804bd241f1364e5a1a3feb6
gdi2290 <github@gdi2290.com>
2013-10-22 21:43:54 -0700

    toggle class in main div

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,55 +5,68 @@
 app.config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'views/main.html'
+      templateUrl: 'views/main.html',
+      wide: true
     })
     .when('/trackpost', {
       templateUrl: 'views/post.html',
       controller: 'PostCtrl as post',
-      activeTab: 'trackpost'
+      activeTab: 'trackpost',
+      wide: false
     })
-    .when('/trackpost/:post/:title', {
+    .when('/trackpost/:subreddit/comments/:post/:title', {
       templateUrl: 'views/trackpost.html',
       controller: 'TrackPostCtrl as post',
-      activeTab: 'trackpost'
+      activeTab: 'trackpost',
+      wide: false
     })
-    .when('/trackpost/:post', {
+    .when('/trackpost/:subreddit/:post', {
       templateUrl: 'views/trackpost.html',
       controller: 'TrackPostCtrl as post',
-      activeTab: 'trackpost'
+      activeTab: 'trackpost',
+      wide: false
     })
     .when('/trackuser', {
       templateUrl: 'views/user.html',
       controller: 'UserCtrl as user',
-      activeTab: 'trackuser'
+      activeTab: 'trackuser',
+      wide: false
     })
     .when('/trackuser/:username', {
       templateUrl: 'views/trackuser.html',
       controller: 'TrackUserCtrl as user',
-      activeTab: 'trackuser'
+      activeTab: 'trackuser',
+      wide: false
     })
     .when('/wordcloud', {
-      templateUrl: 'views/visualizations/wordcloud.html'
+      templateUrl: 'views/visualizations/wordcloud.html',
+      wide: true
     })
     .when('/topiccluster', {
-      templateUrl: 'views/visualizations/topiccluster.html'
+      templateUrl: 'views/visualizations/topiccluster.html',
+      wide: true
     })
     .when('/circlecluster', {
-      templateUrl: 'views/visualizations/circlecluster.html'
+      templateUrl: 'views/visualizations/circlecluster.html',
+      wide: false
     })
     .when('/interaction', {
-      templateUrl: 'views/visualizations/interaction.html'
+      templateUrl: 'views/visualizations/interaction.html',
+      wide: false
     })
     .when('/frequency', {
-      templateUrl: 'views/visualizations/frequency.html'
+      templateUrl: 'views/visualizations/frequency.html',
+      wide: false
     })
     .when('/graphs', {
-      templateUrl: 'views/visualizations/graphs.html'
+      templateUrl: 'views/visualizations/graphs.html',
+      wide: true
     })
     .when('/about', {
       templateUrl: 'views/about.html',
       controller: 'AboutCtrl',
-      activeTab: 'about'
+      activeTab: 'about',
+      wide: false
     })
     .otherwise({
       redirectTo: '/'
--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -6,8 +6,7 @@ app.controller('PostCtrl', function($scope, $location) {
   $scope.submitLink = function(url, index) {
     url = url.split('/');
     index = url.indexOf('comments');
-    url = url.slice(index+1).join('/');
-
+    url = url.slice(index-1).join('/');
     $location.path('/trackpost/'+url);
   };
 
