ef84c235722cf1f14ec4d4e8278331fb3cffdacb
gdi2290 <github@gdi2290.com>
2013-12-15 15:54:15 -0800

    add controller clouds

---
+++ controllers/clouds.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+module.exports = {
+  getCloud: function(req, res, next) {
+    var model = mongoose.model(req.params.collectionName);
+    console.log('requested limit:', req.query.limit);
+    model.find({}, 'noun frequency', {lean: true, limit: req.query.limit }, function (err, docs) {
+      if(err){
+        console.log('from find error: ', JSON.stringify(err));
+      }
+      res.send(docs);
+    });
+  }
+};
