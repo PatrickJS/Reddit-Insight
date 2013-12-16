51901ee3d78abdb4b47980ad9e7b04e494bda9f8
gdi2290 <github@gdi2290.com>
2013-12-15 16:16:50 -0800

    refactor directory structure

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 // Module dependencies.
-var http = require('http'),
-    allPostsCollection = require('./db/pullReddit'),
-    app = require('./config/app')();
+var http = require('http');
+var app = require('./server/config/app')();
 
 // Start server
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port') + ' as ' + process.env.NODE_ENV);
-  if (app.get('env') === 'reddit') {
-    allPostsCollection.start(5000, '/subreddits/popular.json?limit=100', 'subs');
-  }
+  console.log('Reddit Insight listening on port ' + app.get('port') + ' as ' + app.get('env'));
 });
