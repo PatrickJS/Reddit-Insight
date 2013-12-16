e295ed08ab93a8acdcc7195fc60a895bd2d7ad87
gdi2290 <github@gdi2290.com>
2013-12-15 17:28:42 -0800

    update server config env

--- server/config/environments.js
+++ server/config/environments.js
@@ -1,15 +1,28 @@
-var express = require('express'),
-    hbsPrecompiler = require('handlebars-precompiler'),
-    path = require('path');
+var express = require('express');
+var path = require('path');
+var stylus = require('stylus');
 
 module.exports = function(app) {
   // development compile Handlebars and show errors
   app.configure('development', function(){
-    app.set('db-uri', process.env.MONGOLAB_URI || 'mongodb://localhost/WordClouds');
+    app.set('views', path.join(app.SERVER_ROOT, 'server', 'views'));
+    app.set('view engine', 'jade');
+    app.use('/javascripts', express.static(path.join(app.SERVER_ROOT, 'client', 'scripts')));
+    app.use('/stylesheets', express.static(path.join(app.SERVER_ROOT, 'client', 'styles')));
+    app.use('/bower_components', express.static(path.join(app.SERVER_ROOT, 'client', 'bower_components')));
+    app.use(stylus.middleware({
+      src: app.SERVER_ROOT + '/client',
+      dest: app.SERVER_ROOT + '/public',
+      compile: function(str, path, fn) {
+        stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .render(fn);
+      }
+    }));
     app.use(express.errorHandler());
   });
 
   app.configure('production', function(){
-    app.set('db-uri', process.env.MONGOLAB_URI);
   });
 };
