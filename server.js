95e05f948dbaee6d5da115d6b7c0043e13f95ae0
gdi2290 <github@gdi2290.com>
2014-01-12 14:58:31 -0800

    remove http from server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // Module dependencies.
-var http = require('http');
 var app = require('./server/config/app')(__dirname);
 
 // Start server
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
   console.log('Reddit Insight listening on port ' + app.get('port') + ' as ' + app.get('env'));
 });
